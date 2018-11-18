c1861943a81a0bd39c3c36c73e7850bbe65f0aec
izzyge <geisabella@gmail.com>
2018-11-18 06:04:27 -0800

    petition link

--- SchoolSpotlight/models/suggestion.js
+++ SchoolSpotlight/models/suggestion.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const SuggestionSchema = new Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
-  points: { type: Number, default: 0 }
+  points: { type: Number, default: 0 },
+  petition: { type: String, required: false}
 });
 
 module.exports = mongoose.model('Suggestion', SuggestionSchema);
